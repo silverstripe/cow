6e3eb699357fafbd685f59a58681a75a9e26dff5
Damian Mooyman <damian@silverstripe.com>
2016-10-07 18:45:21 +1300

    Add another test

--- tests/Model/Release/ComposerConstraintTest.php
+++ tests/Model/Release/ComposerConstraintTest.php
@@ -138,6 +138,7 @@ class ComposerConstraintTest extends PHPUnit_Framework_TestCase
             ['4.1.3', '3.x-dev', '4.1.x-dev'],
             ['4.1.3', '~3.2.0', '~4.1'],
             ['4.1.3', '^3.2.0', '^4.1'],
+            ['4.1.3', '~3.2', '~4.1'],
 
             // Minor version needs increment
             ['3.2.3', '~3.1.0', '~3.2.0'],
