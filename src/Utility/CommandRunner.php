806dc90c349173c98ee6cc76ea637d311f4d976a
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2022-07-20 11:25:17 +1200

    MNT Run phpcbf to fix linting issues (#222)
    * MNT Run phpcbf to fix linting issues
    
    * DEP require minimum version that supports guzzle 7

--- src/Utility/CommandRunner.php
+++ src/Utility/CommandRunner.php
@@ -10,7 +10,6 @@ use Symfony\Component\Process\Process;
  */
 interface CommandRunner
 {
-
     /**
      * Run the command
      *
--- src/Utility/Translations.php
+++ src/Utility/Translations.php
@@ -10,7 +10,6 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Translations
 {
-
     /**
      * Generate javascript for all modules
      *
